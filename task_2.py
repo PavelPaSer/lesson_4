dd5e18e8fb4a87ae42df99e63e580990fa15107c
«Pashkov <pashkov921326@gmail.com>
2022-10-10 23:15:10 +0400

    выполнил только 3 задачи, 1 задачу не смог решить правильно

--- task_2.py
+++ task_2.py
@@ -3,7 +3,7 @@
 
 # - при N=236     ->        [2, 2, 59]
 
-number = int(input("Integer: "))
+number = int(input("Введите число N: "))
 while number > 1:
     for i in range(2, number + 1):
         if(number % i == 0):
