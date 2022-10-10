77354d5f30818bc7734142a4704a9b1de282e73f
«Pashkov <pashkov921326@gmail.com>
2022-10-10 23:13:16 +0400

    выполнил только 3 задачи, 1 задачу не смог решить правильно

---
+++ task_2.py
@@ -0,0 +1,25 @@
+# 2 Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+# *Пример*
+
+# - при N=236     ->        [2, 2, 59]
+
+number = int(input("Integer: "))
+while number > 1:
+    for i in range(2, number + 1):
+        if(number % i == 0):
+            number //= i
+            print(i)
+            break
+
+
+
+
+# question = int(input("Введите число N: "))
+# answer = []
+# while question > 1:
+#     for i in range(2, question + 1):
+#         if question % i == 0:
+#             answer.append(i)
+#             question = int(question / i)
+#             break
+# print(answer)
